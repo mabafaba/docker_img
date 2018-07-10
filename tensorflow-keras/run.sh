f1c7c00a721fbbd41228a0dd0641466dfbaf6394
mabafaba <barner.mar@gmail.com>
2018-07-10 23:32:52 +0200

    paths updated

--- tensorflow-keras/run.sh
+++ tensorflow-keras/run.sh
@@ -2,9 +2,9 @@
 
 # Set your environment variables before running the container -
 # You shouldn't have any whitespaces or underscores in mount paths:
-export NOTEBOOK_DIR=/Users/m/notebooks
-export SHELTER_REPO_PATH=/Users/m/classifyshelters
-export SHELTER_DATA_PATH=/Volumes/2/shelterdata
+export NOTEBOOK_DIR=~/notebooks
+export SHELTER_REPO_PATH=~/classifyshelters
+export SHELTER_DATA_PATH=~/shelterdata
 
 sudo docker run   \
     --privileged  \
